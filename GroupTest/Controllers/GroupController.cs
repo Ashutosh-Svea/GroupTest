0d9f7b38f9d93de656617d877ba1b5bd62389c47
Jesse Varis <jessevaris@hotmail.com>
2022-01-26 16:36:11 +0100

    removed some lines

--- GroupTest/Controllers/GroupController.cs
+++ GroupTest/Controllers/GroupController.cs
@@ -3,9 +3,6 @@
     public class GroupController
     {
         int number = 7;
-        string hej = "hej";
-        string hej2 = "hej";
-        string hej3 = "hej";
-        string hej4 = "hej";
+
     }
 }
